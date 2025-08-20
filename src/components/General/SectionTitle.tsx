5181ec1502e8749cbfa69814b2dfc8ebfcbf23e2
alvinchristiand23 <alvinchristiand23@gmail.com>
2025-08-20 21:35:52 +0800

    style(layout): change sidebar and content layout

--- src/components/General/SectionTitle.tsx
+++ src/components/General/SectionTitle.tsx
@@ -9,9 +9,8 @@ interface Props {
 export default function SectionTitle({ title, sectionNumber, className }: Props) {
   return (
     <div className={cn("bg-primary sticky top-8 z-20 pb-8", className)}>
-      <div className="border-border flex flex-col border-b-2 px-4 pb-8">
+      <div className="border-border flex flex-col border-b-2 pb-8">
         <p className="supporting-text">Section {sectionNumber}</p>
-
         <h2 className="heading-section">{title}</h2>
       </div>
     </div>
--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,20 +1,15 @@
 import React from "react";
 
-import Divider from "../General/Divider";
 import Footer from "./Footer";
 import Header from "./Header";
 import Navigation from "./Navigation";
 
 export default function Sidebar() {
   return (
-    <aside>
-      <div className="scrollbar-none flex grow flex-col gap-y-1 overflow-y-auto px-4 py-2 lg:gap-y-0 lg:py-4">
-        <Header />
-        <Divider className="my-8 hidden lg:flex" />
-        <Navigation />
-        <Divider className="my-8 hidden lg:flex" />
-        <Footer />
-      </div>
+    <aside className="scrollbar-none">
+      <Header />
+      <Navigation />
+      <Footer />
     </aside>
   );
 }
